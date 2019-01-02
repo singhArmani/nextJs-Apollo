17fe5ee3fb16e0923ab7e515d13ecab7e9a3dfd4
Amandeep singh <amandeep.singh@4mation.com.au>
2019-01-02 15:23:47 +0800

    Passing data via Query strings

--- components/ResourceList/components/Resource/index.jsx
+++ components/ResourceList/components/Resource/index.jsx
@@ -2,7 +2,7 @@ import Link  from 'next/link'
 const Resource = (props) => {
   return (
     <li>
-      <Link href={`/post?title=${props.title}`}>
+      <Link href={`/resource?title=${props.title}`}>
         <a>{props.title}</a>
       </Link>
     </li>
---
+++ pages/resource.js
@@ -0,0 +1,18 @@
+import { withRouter} from 'next/router';
+
+import Layout from '../components/Layout';
+
+const Content = withRouter(props => (
+    <div>
+        <h1>{props.router.query.title}</h1>
+        <p>This is resource details page.</p>
+    </div>
+));
+
+const Page = props => (
+    <Layout>
+        <Content />
+    </Layout>
+);
+
+export default Page;
