ae665aceb4208f6c0db9a850ff5a13001b9ecbbf
Amandeep singh <amandeep.singh@4mation.com.au>
2019-01-02 21:33:56 +0800

    applied CSS in JSS using built in style-jsx babel plugin

--- components/ResourceList/components/Resource/index.jsx
+++ components/ResourceList/components/Resource/index.jsx
@@ -5,6 +5,22 @@ const Resource = (props) => {
       <Link as={`/r/${props.id}`} href={`/resource?title=${props.title}`}>
         <a>{props.title}</a>
       </Link>
+      <style jsx>{`
+        li {
+          list-style: none;
+          margin: 5px 0;
+        }
+  
+        a {
+          text-decoration: none;
+          color: blue;
+          font-family: "Arial";
+        }
+  
+        a:hover {
+          opacity: 0.6;
+        }
+      }`}</style>
     </li>
   )
 }
--- pages/shows.js
+++ pages/shows.js
@@ -2,21 +2,50 @@ import Link from 'next/link';
 import Layout from '../components/Layout';
 import fetch from 'isomorphic-unfetch';
 
+const Show = ({show}) => (
+    <li key={show.id}>
+    <Link as ={`/s/${show.id}`} href={`/show?id=${show.id}`}>
+        <a>
+            {show.name}
+        </a>
+    </Link>
+    <style jsx>{`
+      li {
+        list-style: none;
+        margin: 5px 0;
+      }
+
+      a {
+        text-decoration: none;
+        color: blue;
+        font-family: "Arial";
+      }
+
+      a:hover {
+        opacity: 0.6;
+      }
+    `}</style>
+</li>
+)
+
 function Shows(props) {
   return (
     <Layout>
       <h1>TV Shows</h1>
       <ul>
         {props.shows.map(({show}) => (
-            <li key={show.id}>
-                <Link as ={`/s/${show.id}`} href={`/show?id=${show.id}`}>
-                    <a>
-                        {show.name}
-                    </a>
-                </Link>
-            </li>
+           <Show key={show.id} show={show} />
         ))}
       </ul>
+      <style jsx>{`
+      h1, a {
+        font-family: "Arial";
+      }
+
+      ul {
+        padding: 0;
+      }
+    `}</style>
     </Layout>
   )
 }
